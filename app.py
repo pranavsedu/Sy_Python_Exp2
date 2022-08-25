c36f7ca8dc623ca3b679bf848f54d55ae1b61a85
Pranav-pce <95762410+Pranav-pce@users.noreply.github.com>
2022-08-25 16:00:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,44 @@
-number = int(input("Enter a number > "))
-string = input("Enter a string > ")
 
+go_on = True
 
-n = int(len(string) / 2)
-string2 = string[0:n]
-string3 = string[len(string) - n:]
-rev_string3 = string3[len(string3)::-1]
+while go_on:
+	
+	print()
+	# Menu to choose operations
+	print("Choose an option from the menu")
+	print("1 : Check For Palindrome\n2 : Calculate Factorial\n3 : Exit")
+	option_choosen = int(input("Enter an option > "))
 
-if string2 == rev_string3:
-    print("Palindrome!")
-else:
-    print("Not A Palindrome!")
+	# Parsing the input
+	if option_choosen == 1:
+		# Palandrome
+		string = input("Enter a string > ")
+		n = int(len(string) / 2)
+		string2 = string[0:n]
+		string3 = string[len(string) - n:]
+		rev_string3 = string3[len(string3)::-1]
+		
+		
+		if string2.lower() == rev_string3.lower():
+		    print("Palindrome!")
+		else:
+		    print("Not A Palindrome!")
+	
+	elif option_choosen == 2:
+		# Factorial
+		number = int(input("Enter a number > "))
+		if number == 0:
+			print("Factorial = ", number)
+		elif number > 0:
+			factorial = 1;
+			for i in range(1,(number + 1)):
+				factorial *= i;
+			print("Factotial of ", number , " is ", factorial)
+		elif number < 0:
+			print("For calculation of factorial number must be +ve or zero(0), you entered a -ve number : ", number)
+		else:
+			print("Something went wrong")
+	elif option_choosen == 3:
+		# Exit      
+		go_on = False
+
